0708c539a75b4807cb61a776a4f869c6f2d61075
FabrizioAdi <adriannoszczyk@gmail.com>
2022-01-22 17:39:54 +0000

    add category filter function in views.py file

--- Robinson/urls.py
+++ Robinson/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Trailers.views import index
-
+from Trailers.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index),
+    path('',index, name ='index'),
+    path('category/<id>/', category, name='category'),
+
 ]
--- Trailers/views.py
+++ Trailers/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Trailers
+from .models import Trailers, Category
 
 def index(request):
-    question = Trailers.objects.all()
+    question = Category.objects.all()
     return HttpResponse(question)
+
+def category(request, id):
+    category_user = Category.objects.get(pk=id)
+    return HttpResponse(category_user.name)
 
 # one = Trailers.objects.all(pk=2) pk-primary key
 # return HttpResponse(one)
