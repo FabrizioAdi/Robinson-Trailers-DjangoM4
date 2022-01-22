c5906ec71cfbe27ef4e4d6c00c63565629784d07
FabrizioAdi <adriannoszczyk@gmail.com>
2022-01-22 14:02:34 +0000

    Create superuser, and first models in models.py file

--- Trailers/models.py
+++ Trailers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# My models here.
+
+class Trailers(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    excerpt = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    
