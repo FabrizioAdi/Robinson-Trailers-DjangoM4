95384600f059ee443fc4647ee7180454ba9ea979
FabrizioAdi <adriannoszczyk@gmail.com>
2022-01-22 15:15:18 +0000

    Add sector model in models.py file, checking the operation of the website from the admin point

--- Trailers/admin.py
+++ Trailers/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Trailers
+from .models import Trailers, Sector
 
 # Register your models here.
 admin.site.register(Trailers)
+admin.site.register(Sector)
--- Trailers/models.py
+++ Trailers/models.py
@@ -2,13 +2,35 @@ from django.db import models
 
 # My models here.
 
+# Sector models
+
+class Sector(models.Model):
+
+    def __str__(self):
+        return self.title
+
+    title = models.CharField(max_length=60)
+    excerpt = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = "Sector"
+        verbose_name_plural = "Sectors"
+
+# Trailers model.
+
 class Trailers(models.Model):
+
+    def __str__(self):
+        return self.title
+
+    sector = models.ForeignKey(Sector, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=100, unique=True)
     excerpt = models.TextField(blank=True)
     price = models.DecimalField(max_digits=12, decimal_places=2)
-    def __str__(self):
-        return self.title
 
     class Meta:
         verbose_name = "Trailer"
         verbose_name_plural = "Trailers"
+
+
+
