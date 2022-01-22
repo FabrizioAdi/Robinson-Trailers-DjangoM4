fe82a7b5e899563f43aff30a41d8cbef21354c4c
FabrizioAdi <adriannoszczyk@gmail.com>
2022-01-22 16:05:49 +0000

    Playing with diffrent type of filters, add some comments

--- Trailers/views.py
+++ Trailers/views.py
@@ -5,3 +5,9 @@ from .models import Trailers
 def index(request):
     question = Trailers.objects.all()
     return HttpResponse(question)
+
+# one = Trailers.objects.all(pk=2) pk-primary key
+# return HttpResponse(one)
+
+# filter ------ cat = Trailers.objects.filter(category=2)
+# return HttpResponse(cat)
